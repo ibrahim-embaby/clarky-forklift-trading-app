a1614025ce49fa8e82e9bc06cdb68250778dfdab
ibrahim-embaby <ibrahimembaby10@gmail.com>
2024-05-28 13:37:05 +0300

    adding meta data for other site pages

---
+++ client/netlify/edge-functions/site-preview.js
@@ -0,0 +1,20 @@
+// netlify/functions/ad-meta-tags.js
+export default async (request, context) => {
+  try {
+    const responsepage = await context.next();
+    const page = await responsepage.text();
+
+    const updatedPage = page
+      .replace("__META_TITLE__", "Clarky | كلاركي")
+      .replace("__META_DESCRIPTION__", "Forklift Trading Platform")
+      .replace("__META_IMAGE__", "%PUBLIC_URL%/images/logo-q.png");
+
+    return new Response(updatedPage, responsepage);
+  } catch (error) {
+    console.error("Error in Edge Function:", error);
+    return {
+      statusCode: 500,
+      body: "Internal Server Error",
+    };
+  }
+};
