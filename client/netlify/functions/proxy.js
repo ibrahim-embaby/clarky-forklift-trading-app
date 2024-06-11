15b8194ce904d9130900eb23008b31b093af1521
ibrahim-embaby <ibrahimembaby10@gmail.com>
2024-06-11 16:55:10 +0300

    trying to setup a proxy

---
+++ client/netlify/functions/proxy.js
@@ -0,0 +1,18 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function (event, context) {
+  const response = await fetch(
+    "http://clarky.eu-north-1.elasticbeanstalk.com" + event.path,
+    {
+      method: event.httpMethod,
+      headers: event.headers,
+      body: event.body,
+    }
+  );
+
+  const data = await response.json();
+  return {
+    statusCode: response.status,
+    body: JSON.stringify(data),
+  };
+};
