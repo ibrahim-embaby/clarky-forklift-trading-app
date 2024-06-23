da55f2ad2766280fed1f5962d1600ddef935e981
ibrahim-embaby <ibrahimembaby10@gmail.com>
2024-06-23 18:38:49 +0300

    removed linkedin icon

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,7 +1,6 @@
 import "./footer.css";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import Instagram from "@mui/icons-material/Instagram";
-import LinkedIn from "@mui/icons-material/LinkedIn";
 import { useTranslation } from "react-i18next";
 function Footer() {
   const { t } = useTranslation();
@@ -15,14 +14,6 @@ function Footer() {
         <p className="footer-right-icon instagram">
           <Instagram sx={{ fontSize: 35 }} />
         </p>
-        <a
-          href="https://www.linkedin.com/in/ibrahim-embaby/"
-          target="_blank"
-          rel="noreferrer"
-          className="footer-right-icon linkedin"
-        >
-          <LinkedIn sx={{ fontSize: 35 }} />
-        </a>
       </div>
     </div>
   );
