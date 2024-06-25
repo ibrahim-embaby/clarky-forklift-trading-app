988e030e9bb74237fa420dc4815bcfdf9af65244
ibrahim-embaby <ibrahimembaby10@gmail.com>
2024-06-25 18:57:43 +0300

    added facebook page link

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,6 +1,5 @@
 import "./footer.css";
 import FacebookIcon from "@mui/icons-material/Facebook";
-import Instagram from "@mui/icons-material/Instagram";
 import { useTranslation } from "react-i18next";
 function Footer() {
   const { t } = useTranslation();
@@ -8,12 +7,13 @@ function Footer() {
     <div className="footer">
       <div className="footer-left">{t("footer_title")}</div>
       <div className="footer-right">
-        <p className="footer-right-icon facebook">
+        <a
+          href="https://www.facebook.com/clarky.eg"
+          target="_blank"
+          className="footer-right-icon facebook"
+        >
           <FacebookIcon sx={{ fontSize: 35 }} />
-        </p>
-        <p className="footer-right-icon instagram">
-          <Instagram sx={{ fontSize: 35 }} />
-        </p>
+        </a>
       </div>
     </div>
   );
