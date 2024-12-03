063776733dacf2680e799916911125c32784464c
Ibrahim Mohamed Embaby <54782271+ibrahim-embaby@users.noreply.github.com>
2024-12-03 17:11:42 +0200

    Update allowedOrigins.js

--- server/utils/allowedOrigins.js
+++ server/utils/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://clarky-eg.netlify.app",
   "http://localhost:5173",
-  "https://clarky.site/"
+  "https://clarky.site"
 ];
 
 module.exports = allowedOrigins;
